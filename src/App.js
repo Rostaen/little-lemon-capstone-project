d8fed631c87a7c290cc6eeba42e3e0d0acec35b7
Andrew Fillhart <afillhar@bard.edu>
2023-12-20 11:00:23 -0500

    Adding meta tags and setting up OGP

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Helmet from './components/Helmet';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Helmet />
       <Header />
       <Nav />
       <Main />
---
+++ src/components/Helmet.js
@@ -0,0 +1,10 @@
+import {Helmet} from 'react-helmet';
+
+const Helmet = () => {
+    <Helmet>
+        <meta name='description' content='Little Lemon description here.' />
+        <meta name='og:title' content='Little Lemon' />
+        <meta name='og:description' content='Little Lemon description here.' />
+        <meta name='og:link' content='../images/little-lemon-social-box-img.jpg' />
+    </Helmet>
+};
